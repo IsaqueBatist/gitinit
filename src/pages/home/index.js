c24ef9a7baaf62fc831e31402c48830d97a51cef
Isaque Barbosa <isaquebatista400@gmail.com>
2024-05-01 12:03:45 -0300

    Adding picture and describption

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -12,6 +12,17 @@ function App() {
           <div>
             <input name="usuario" placeholder="@username"/>
             <button>Buscar</button>
+            <div>
+            <div className="perfil">
+                <img src="https://avatars.githubusercontent.com/u/160277060?v=4" className="profile" alt="profile"/>
+                <div>
+                  <h3>Isaque Barbosa</h3>
+                  <span>@isaqueBatist</span>
+                  <p>Descrição</p>
+                </div>
+              </div>
+              <hr/>
+            </div>
           </div>
         </div>
       </div>
